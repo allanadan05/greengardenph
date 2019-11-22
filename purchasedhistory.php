f38de6635fc36cd84fb3de9f9beaadf99de24e6b
stevenigop <57278961+stevenigop@users.noreply.github.com>
2019-11-23 01:50:46 +0800

    users panel

---
+++ purchasedhistory.php
@@ -0,0 +1,300 @@
+<head>
+    <title>GreenGarden</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="./import/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="./import/css/animate.css">
+
+    <link rel="stylesheet" href="./import/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="./import/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="./import/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="./import/css/aos.css">
+
+    <link rel="stylesheet" href="./import/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="./import/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="./import/css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="./import/css/flaticon.css">
+    <link rel="stylesheet" href="./import/css/icomoon.css">
+    <link rel="stylesheet" href="./import/css/style.css">
+    <link rel="stylesheet" href="./angelicoimport/angelicostyles.css">
+    <link rel="stylesheet" href="./stevenimport/style.css">
+
+</head>
+
+<body class="goto-here">
+    <div class="py-1 bg-primary">
+        <div class="container">
+            <div class="row no-gutters d-flex align-items-start align-items-center px-md-0">
+                <div class="col-lg-12 d-block">
+                    <div class="row d-flex">
+                        <div class="col-md pr-4 d-flex topper align-items-center">
+                            <div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+                            <span class="text">+63 945 0928 187</span>
+                        </div>
+                        <div class="col-md pr-4 d-flex topper align-items-center">
+                            <div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+                            <span class="text">greengarden@gmail.com</span>
+                        </div>
+                        <div class="col-md-5 pr-4 d-flex topper align-items-center text-lg-right">
+                            <span class="text">3-5 Business days delivery &amp; Free Returns</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">GreenGarden</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Menu
+          </button>
+
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item cta cta-colored"><a href="#" class="nav-link"><span class="icon-bell"></span></a></li>
+                    <li class="nav-item cta cta-colored"><a href="#" class="nav-link"><span class="icon-envelope"></span></a></li>
+                    <li class="nav-item cta cta-colored"><a href="profile.html" class="nav-link"><span class="icon-person"></span> [name]</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- Search Bar -->
+
+
+        <form action="index.html" class="search-form">
+            <form class="form-group">
+                <span class="icon ion-ios-search" style="color: green ;"></span>
+                <input type="text" class="form-control" placeholder="Search..." style="font-size: small;">
+            </form>
+        </form>
+
+    </nav>
+
+    <!-- END nav -->
+
+    <!-- CONTENT -->
+  <div class="container-fluid">
+    <div class="row">
+        <!-- row -->
+        <!-- SIDEBAR -->
+        <div class="col-sm-2">
+            <div class="side-bar">
+                <ul>
+                    <li><a href="/greengardenph/users.php" >Posts</a></li>
+                    <li><a href="/greengardenph/purchasedhistory.php" class="active">Purchased History & E-Wallet</a></li>
+                    <li><a href="/greengardenph/vouchers.php">Vouchers</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- /SIDEBAR -->
+        <!-- TABLE -->
+        <!-- purchased history -->
+        <div class="col-sm-5">
+                <h6>Purchased History</h6>
+                <div class="panel panel-default">
+                    <div class="history">
+                        <div class="row">
+                                <!-- 1st column -->
+                                <div class="col-sm-7">
+                                <label>Purchased from</label>
+                                <p>Paymaya</p>   
+                                </div>
+                                <div class="col-sm-5">
+                                    <label>21 September, 2019</label>
+                                      <p>PHP 47.50</p>
+                                </div>
+                                <!-- /1st column -->
+                                <hr>
+                                 <!-- 2st column -->
+                                 <div class="col-sm-7">
+                                <label>Purchased from</label>
+                                <p>Paymaya</p>   
+                                </div>
+                                <div class="col-sm-5">
+                                    <label>21 September, 2019</label>
+                                      <p>PHP 47.50</p>
+                                </div>
+                                <!-- /2st column -->
+                                <hr>
+                                 <!-- 3st column -->
+                                 <div class="col-sm-7">
+                                <label>Purchased from</label>
+                                <p>Paymaya</p>   
+                                </div>
+                                <div class="col-sm-5">
+                                    <label>21 September, 2019</label>
+                                     <p>PHP 47.50</p>
+                                </div>
+                                <!-- /3st column -->
+                                <!-- 3st column -->
+                                <div class="col-sm-7">
+                                <label>Purchased from</label>
+                                <p>Paymaya</p>   
+                                </div>
+                                <div class="col-sm-5">
+                                    <label>21 September, 2019</label>
+                                      <p>PHP 47.50</p>
+                                </div>
+                                <!-- /3st column -->
+                        </div>
+                    </div>
+                </div>
+
+           
+        </div>
+        <!-- /purchased history -->
+        <!-- E-wallet -->
+        <div class="col-sm-5">
+        <h6>Green Garden E-Wallet</h6>
+                <div class="panel panel-default">
+                    <div class="e-wallet">
+                        <p id="peso">₱204.50</p>
+                        <br>
+                        <center>
+                        <label>One Click</label>
+                        <label>Get Rebates</label>
+                        <label>Earn Rebates</label>
+                        </center>
+                        <center>
+                        <img src="images/oneclick.png">
+                        <img src="images/earnrebates.png">
+                        <img src="images/getrebates.png">
+                        </center>
+                        
+                    </div>
+                </div>
+
+        </div>
+        <!-- /E-wallet -->
+
+        
+        
+        
+    </div>
+    <!-- /row -->
+</div>
+    <!-- /CONTENT -->
+
+
+
+
+    <!-- Footer -->
+
+    <footer class="ftco-footer ftco-section">
+    <div class="container">
+        <div class="row">
+            <div class="mouse">
+                <a href="#" class="mouse-icon">
+                    <div class="mouse-wheel"><span class="ion-ios-arrow-up"></span></div>
+                </a>
+            </div>
+        </div>
+        <div class="row mb-5">
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">GreenGarden</h2>
+                    <p>Far far away, behind the word mountains, far from the countries Philippines and China.</p>
+                    <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4 ml-md-5">
+                    <h2 class="ftco-heading-2">Menu</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="py-2 d-block">Home</a></li>
+                        <li><a href="#" class="py-2 d-block">Products</a></li>
+                        <li><a href="#" class="py-2 d-block">Category</a></li>
+                        <li><a href="#" class="py-2 d-block">Feed</a></li>
+                        <li><a href="#" class="py-2 d-block">Cart</a></li>
+                        <li><a href="#" class="py-2 d-block">Wishlist</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Help</h2>
+                    <div class="d-flex">
+                        <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
+                            <li><a href="#" class="py-2 d-block">Shipping Information</a></li>
+                            <li><a href="#" class="py-2 d-block">Returns &amp; Exchange</a></li>
+                            <li><a href="#" class="py-2 d-block">Terms &amp; Conditions</a></li>
+                            <li><a href="#" class="py-2 d-block">Privacy Policy</a></li>
+                        </ul>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-2 d-block">FAQs</a></li>
+                            <li><a href="#" class="py-2 d-block">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Have a Questions?</h2>
+                    <div class="block-23 mb-3">
+                        <ul>
+                            <li><span class="icon icon-map-marker"></span><span class="text">Bulacan State Univerysity, Brgy. Kaypian, CSJDM, Bulacan 3023</span></li>
+                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+63 945 0928 187</span></a></li>
+                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">greengarden@gmail.com</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+
+                <p>
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>
+                    All rights reserved | This website is made for completion of Advanced Web Application requirement
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+
+<!-- loader -->
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+
+
+<script src="./import/js/jquery.min.js"></script>
+<script src="./import/js/jquery-migrate-3.0.1.min.js"></script>
+<script src="./import/js/popper.min.js"></script>
+<script src="./import/js/bootstrap.min.js"></script>
+<script src="./import/js/jquery.easing.1.3.js"></script>
+<script src="./import/js/jquery.waypoints.min.js"></script>
+<script src="./import/js/jquery.stellar.min.js"></script>
+<script src="./import/js/owl.carousel.min.js"></script>
+<script src="./import/js/jquery.magnific-popup.min.js"></script>
+<script src="./import/js/aos.js"></script>
+<script src="./import/js/jquery.animateNumber.min.js"></script>
+<script src="./import/js/bootstrap-datepicker.js"></script>
+<script src="./import/js/scrollax.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+<script src="./import/js/google-map.js"></script>
+<script src="./import/js/main.js"></script>
+
+
+</body>
+</html>
