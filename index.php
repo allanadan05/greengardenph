7ca03703dbbcdf09dd153aa4de44f3bc50f48cba
Allan Adan <allanadan1999@gmail.com>
2019-11-22 21:49:04 -0800

    Update index.php

--- index.php
+++ index.php
@@ -220,7 +220,7 @@
                             </div>
                             <div class="bottom-area d-flex px-3">
                                 <div class="m-auto d-flex">
-                                    <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
+                                    <a href="product-single.php" class="add-to-cart d-flex justify-content-center align-items-center text-center">
                                         <span><i class="ion-ios-menu"></i></span>
                                     </a>
                                     <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
@@ -436,26 +436,6 @@
             </div>
         </div>
     </section>
-
-    <!-- <section class="ftco-section img" style="background-image: url(images/bg_3.jpg);">
-        <div class="container">
-            <div class="row justify-content-end">
-                <div class="col-md-6 heading-section ftco-animate deal-of-the-day ftco-animate">
-                    <span class="subheading">Best Price For You</span>
-                    <h2 class="mb-4">Deal of the day</h2>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                    <h3><a href="#">Spinach</a></h3>
-                    <span class="price">$10 <a href="#">now $5 only</a></span>
-                    <div id="timer" class="d-flex mt-5">
-                        <div class="time" id="days"></div>
-                        <div class="time pl-3" id="hours"></div>
-                        <div class="time pl-3" id="minutes"></div>
-                        <div class="time pl-3" id="seconds"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> -->
 
     <section class="ftco-section testimony-section">
         <div class="container">
@@ -755,6 +735,9 @@
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
     <script src="./import/js/google-map.js"></script>
     <script src="./import/js/main.js"></script>
+
+
+
 
 </body>
 
