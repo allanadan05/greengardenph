a635b673d8edaec1244287a98416a129d5324b40
johnriel16 <57279272+johnriel16@users.noreply.github.com>
2019-11-23 14:41:35 +0200

    may php na

--- navbar.php
+++ navbar.php
@@ -1,5 +1,29 @@
 
+        <?php
+session_start();
+$name="";
 
+if(isset($_POST["login"])){
+    $con= new mysqli("localhost","root","","project");
+
+    $email = $con->real_escape_string($_POST["fname"]);
+    $userpass = $con->real_escape_string($_POST["userpass"]);
+    $data = $con->query("Select Lname from usertbl where fname='$email' AND userpass='$userpass'" );
+    if($data ->num_rows>0)
+    {
+        
+            while($sql=$data ->fetch_array()){
+                $name=$sql["Lname"];
+        }
+  
+    }
+    else {
+        echo "incorrect email or password";
+    }
+
+}
+
+?>
 <div class="py-1 bg-primary">
         <div class="container">
             <div class="row no-gutters d-flex align-items-start align-items-center px-md-0">
@@ -47,6 +71,7 @@
                     <!-- <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li> -->
                     <li class="nav-item cta cta-colored"><a href="wishlist.php" class="nav-link"><span class="icon-heart"></span>[0]</a></li>
                     <li class="nav-item cta cta-colored"><a href="cart.php" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
+                        
                     <li class="nav-item cta cta-colored"><a name="prof" id="login-btn" class="nav-link"><?php
                     echo $name;
                     ?> <span class="icon-person">
