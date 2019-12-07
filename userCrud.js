878d8bec1a029273668dd07a76ec5ace692f606f
jariCruz <jhari.cruz.jc@gmail.com>
2019-12-07 11:03:16 +0800

    update function

--- userCrud.js
+++ userCrud.js
@@ -1,8 +1,9 @@
+//add
 function insert() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("response").innerHTML = this.responseText;
+      document.getElementById("addResponse").innerHTML = this.responseText;
       document.getElementById("fname").value="";
       document.getElementById("lname").value="";
       document.getElementById("mname").value="";
@@ -27,3 +28,50 @@ function insert() {
     xhttp.send();
 
 }
+
+//edit and update
+function edit(ipinasa){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+              var buongObject=JSON.parse(this.responseText);
+              document.getElementById("ufname").value = buongObject.ufname;
+              document.getElementById("ulname").value = buongObject.ulname;
+              document.getElementById("umname").value = buongObject.umname;
+              document.getElementById("uage").value = buongObject.uage;
+              document.getElementById("ugender").value = buongObject.ugender;
+              document.getElementById("uaddress").value = buongObject.uaddress;
+              document.getElementById("uemail").value = buongObject.uemail;
+              document.getElementById("upword").value = buongObject.upword;
+              document.getElementById("userid").value = forIpinasa;
+      }
+    };
+
+      var forIpinasa = ipinasa;
+      console.log(forIpinasa);
+      var token = "edit";
+      xhttp.open("GET", "userCrud.php?forIpinasa="+forIpinasa+"&token="+token, true);
+      xhttp.send();
+  }
+  function update() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        document.getElementById("updateResponse").innerHTML = this.responseText;
+      }
+    };
+    var ufname = document.getElementById("ufname").value;
+    var ulname = document.getElementById("ulname").value;
+    var umname = document.getElementById("umname").value;
+    var uage = document.getElementById("uage").value;
+    var ugender = document.getElementById("ugender").value;
+    var uaddress = document.getElementById("uaddress").value;
+    var uemail = document.getElementById("uemail").value;
+    var upword = document.getElementById("upword").value;
+    var userid = document.getElementById("userid").value;
+    var token = "update";
+    xhttp.open("GET", "userCrud.php?ufname="+ufname+"&ulname="+ulname+"&umname="+umname+"&uage="+uage+"&ugender="+ugender+
+    "&uaddress="+uaddress+"&uemail="+uemail+"&upword="+upword+"&userid="+userid+"&token="+token, true);
+      xhttp.send();
+
+  }
